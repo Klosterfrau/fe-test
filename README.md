ac46b41c9adf419695e999fcca706772aee2487c
thkle <thomas.klessen@icloud.com>
2018-03-23 10:32:19 +0100

    added Readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# fe-test
-Frontend Bewerber Test
+# InSitu Frontend Test
+
+Hallo Bewerber.
+
+Um diesen Test zu absolvieren benötigst Du einen Github Account, git und node.js.
+
+Bitte forke dir das Projekt als erstes in deinen eigenen Github Account.
+
+Nachdem du es dann auf deinen Rechner geclont hast musst du einmal
+
+`npm install`
+
+ausführen und kannst danach den dev-server mit
+
+`npm run serve`
+
+starten.
+
+Viel Erfolg!
+
--- src/store.js
+++ src/store.js
@@ -14,40 +14,40 @@ Vue.use(Vuex);
 */
 
 const store = new Vuex.Store({
-    state: {
-      cats: {},
-      catList: []
-    },
-    mutations: {
-      addCats (state, cats) {
-          if (!cats) {
-              return
-          }
-          cats.forEach((cat) => {
-              if (state.catList.indexOf(cat.id) === -1) {
-                  state.catList.push(cat.id)
-              }
-              Vue.set(state.cats, cat.id, cat)
-          })
+  state: {
+    cats: {},
+    catList: []
+  },
+  mutations: {
+    addCats(state, cats) {
+      if (!cats) {
+        return;
       }
-    },
-    getters: {
-      getCats (state) {
-        return state.catList.map(catId => state.cats[catId])
-      }
+      cats.forEach(cat => {
+        if (state.catList.indexOf(cat.id) === -1) {
+          state.catList.push(cat.id);
+        }
+        Vue.set(state.cats, cat.id, cat);
+      });
     }
-})
+  },
+  getters: {
+    getCats(state) {
+      return state.catList.map(catId => state.cats[catId]);
+    }
+  }
+});
 
-const mockCats = []
-for (let i=0; i < 20; i++) {
+const mockCats = [];
+for (let i = 0; i < 20; i++) {
   mockCats.push({
-      id: i,
-      name: 'Kitty ' + i,
-      desc: 'Nice Kitty ' + i,
-      votes: 0
-  })
+    id: i,
+    name: "Kitty " + i,
+    desc: "Nice Kitty " + i,
+    votes: 0
+  });
 }
 
-store.commit('addCats', mockCats)
+store.commit("addCats", mockCats);
 
-export default store
+export default store;
